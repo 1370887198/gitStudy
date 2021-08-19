f2dfe21d4982db1952075152d938ccc9ecf69aef
jinkaichao <jinkaichao@kuaishou.com>
2021-08-19 14:33:56 +0800

    first

--- src/test.java
+++ src/test.java
@@ -4,26 +4,4 @@
  */
 public class test {
     private int age;
-	//在worktree
-	//在add
-	//测试push命令
-	//working on branch jinkacihao
-	//jinkaichaobranch中解决了冲突
-	//test2
-	//test3
-<<<<<<< HEAD
-	//修复完bug
-=======
-<<<<<<< HEAD
-	//test git stash
-=======
-	//修复完bug
->>>>>>> 6fbe7e3 (bug修复完成)
-	//测试stash功能
->>>>>>> jinkaichaotest
-	//第一次push
-	//第22222次push
-	//testpull
-	//rebase1
-	//reabse2
 }
